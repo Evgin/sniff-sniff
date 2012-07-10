63dd300cae552391a019fde53b3ade8ce4ff1d27
Evgin <evgin.goncharov@gmail.com>
2012-07-10 11:44:04 +0400

    - log in file

--- log_sandbox.py
+++ log_sandbox.py
@@ -1,10 +1,14 @@
 import sys
+import logging
 
 __author__ = 'e.goncharov'
 
 
 def main():
-    pass
+    logging.basicConfig(level=logging.WARNING, filename='root.log', )
+    logging.warning('Watch out!') # will print a message to the console
+    logging.info('I told you so') # will not print anything
+
 
 if __name__ == '__main__':
     sys.exit(main())
